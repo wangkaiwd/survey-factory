a92c08f41f65daab76170d5d9e8bea0b3bd90438
wangkaiwd <1162486176@qq.com>
2025-05-11 15:10:38 +0800

    feat: integrate with zustand

--- src/app/editor/components/EditorCanvas.tsx
+++ src/app/editor/components/EditorCanvas.tsx
@@ -6,6 +6,8 @@ import TextContainer from '@/components/TextContainer'
 import BlockContainer from '@/components/BlockContainer'
 import { blockMap } from '@/blocks'
 import { FormProvider, useForm } from 'react-hook-form'
+import { useQuestionStore } from '@/store/useQuestionStore'
+import { mockData } from '@/mock'
 
 interface FormTextStyle {
   align: AlignType
@@ -20,29 +22,18 @@ interface FormState {
 }
 
 const EditorCanvas = () => {
-  const [activeBlockId, setActiveBlockId] = useState<string | undefined>(undefined)
+  const setQuestions = useQuestionStore((state) => state.setQuestions)
+  const setPageInfo = useQuestionStore((state) => state.setPageInfo)
+  const questions = useQuestionStore((state) => state.questions)
+  const pageInfo = useQuestionStore((state) => state.pageInfo)
   const blockRefs = useRef<HTMLDivElement[]>([])
-  const [pageInfo, setPageInfo] = useState<FormState>({
-    id: 'page-id-1',
-    title: {
-      align: 'center',
-      text: '标题',
-    },
-    description: {
-      align: 'center',
-      text: '描述',
-    },
-  })
-  const [questions, setQuestions] = useState([
-    {
-      id: 'question-1',
-      type: 'OpenQuestion',
-      props: {
-        title: '问答题',
-        placeholder: '请输入问题',
-      },
-    },
-  ])
+  const setActiveQuestionId = useQuestionStore((state) => state.setActiveQuestionId)
+  const activeQuestionId = useQuestionStore((state) => state.activeQuestionId)
+  useEffect(() => {
+    setQuestions(mockData.questions)
+    setPageInfo(mockData.pageInfo)
+  }, [])
+
   const methods = useForm({
     values: {
       questions,
@@ -50,6 +41,7 @@ const EditorCanvas = () => {
   })
 
   useEffect(() => {
+    console.log('useEffect---watch')
     const { unsubscribe } = methods.watch((value: any) => {
       setQuestions([...value.questions])
     })
@@ -57,7 +49,7 @@ const EditorCanvas = () => {
   }, [methods.watch])
 
   useClickOutside(blockRefs, () => {
-    setActiveBlockId(undefined)
+    setActiveQuestionId(null)
   })
   const addBlockRef = (el: HTMLDivElement | null) => {
     if (!el) {return}
@@ -71,7 +63,6 @@ const EditorCanvas = () => {
 
   const renderBlocks = () => {
     return questions.map((question) => {
-      const editable = activeBlockId === question.id
       const BlockComponent = blockMap.get(question.type)
       if (!BlockComponent) {
         return null
@@ -79,13 +70,12 @@ const EditorCanvas = () => {
       return (
         <BlockContainer
           key={question.id}
+          id={question.id}
           ref={addBlockRef}
-          onClick={() => setActiveBlockId(question.id)}
-          editable={editable}
         >
           <BlockComponent
+            id={question.id}
             {...question.props}
-            editable={editable}
           />
         </BlockContainer>
       )
@@ -95,15 +85,15 @@ const EditorCanvas = () => {
     <div className={'flex-1 bg-gray-100 flex items-center justify-center py-3 h-full'}>
       <div className={'w-[600px] bg-white rounded-md h-full py-6'}>
         <TextContainer
-          editable={activeBlockId === 'title-id'}
+          editable={activeQuestionId === 'title-id'}
           ref={addBlockRef}
-          onClick={() => setActiveBlockId('title-id')}
+          onClick={() => setActiveQuestionId('title-id')}
         >
           <EditableText
-            value={pageInfo.title.text}
-            align={pageInfo.title.align}
+            value={pageInfo.title?.text}
+            align={pageInfo.title?.align}
             placeholder={'请输入标题'}
-            editable={activeBlockId === 'title-id'}
+            editable={activeQuestionId === 'title-id'}
             className={'text-3xl'}
             onChange={(textStyle) => {
               setPageInfo({ ...pageInfo, title: textStyle as FormTextStyle })
@@ -111,16 +101,16 @@ const EditorCanvas = () => {
           />
         </TextContainer>
         <TextContainer
-          editable={activeBlockId === 'description-id'}
+          editable={activeQuestionId === 'description-id'}
           ref={addBlockRef}
           className={'text-neutral-600'}
-          onClick={() => setActiveBlockId('description-id')}
+          onClick={() => setActiveQuestionId('description-id')}
         >
           <EditableText
-            value={pageInfo.description.text}
-            align={pageInfo.description.align}
+            value={pageInfo.description?.text}
+            align={pageInfo.description?.align}
             placeholder={'请输入描述'}
-            editable={activeBlockId === 'description-id'}
+            editable={activeQuestionId === 'description-id'}
             onChange={(textStyle) => { setPageInfo({ ...pageInfo, description: textStyle as FormTextStyle }) }}
           />
         </TextContainer>
--- src/components/BlockContainer/index.tsx
+++ src/components/BlockContainer/index.tsx
@@ -1,13 +1,17 @@
 import { cn } from '@/lib/utils'
 import React, { HTMLAttributes, Ref } from 'react'
+import { useQuestionStore } from '@/store/useQuestionStore'
 
 export interface TextContainerProps extends HTMLAttributes<HTMLDivElement> {
   ref: Ref<HTMLDivElement>;
-  editable?: boolean;
+  id: string;
 }
 
 const BlockContainer = (props: TextContainerProps) => {
-  const { ref, className, editable = false, ...rest } = props
+  const { ref, className, id, ...rest } = props
+  const activeQuestionId = useQuestionStore((state) => state.activeQuestionId)
+  const setActiveQuestionId = useQuestionStore((state) => state.setActiveQuestionId)
+  const editable = activeQuestionId === id
   const getCls = () => {
     const baseCls = 'px-10 pt-3 pb-6'
     if (!editable) {
@@ -16,7 +20,7 @@ const BlockContainer = (props: TextContainerProps) => {
     return cn(baseCls, 'shadow-container border-y border-l-4 border-l-blue-500', className)
   }
   return (
-    <div ref={ref} className={getCls()} {...rest} >
+    <div ref={ref} className={getCls()} {...rest} onClick={() => setActiveQuestionId(id)}>
       {props.children}
     </div>
   )
