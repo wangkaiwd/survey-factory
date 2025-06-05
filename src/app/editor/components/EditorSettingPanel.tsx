5e45c44d3ae10015d0cafd22fb3325910cd924d9
wangkaiwd <1162486176@qq.com>
2025-06-05 10:40:59 +0800

    feat: integrate form component

--- src/app/editor/components/EditorSettingPanel.tsx
+++ src/app/editor/components/EditorSettingPanel.tsx
@@ -1,8 +1,69 @@
+'use client'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { useForm } from 'react-hook-form'
+import { Switch } from '@/components/ui/switch'
+
+const formItemBaseCls = 'flex justify-between'
+const formLabelBaseCls = 'w-20'
+
 const EditorSettingPanel = () => {
+  const form = useForm()
   return (
-    <div className={'w-72 h-full bg-white rounded-md'}>
-      EditorSettingPanel
-    </div>
+    <Card className="w-80 h-full">
+      <CardHeader>
+        <CardTitle>题目设置</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form>
+            <div className={'space-y-4'}>
+              <FormField
+                control={form.control}
+                name="required"
+                render={({ field }) => (
+                  <FormItem className={formItemBaseCls}>
+                    <FormLabel className={formLabelBaseCls}>必填</FormLabel>
+                    <FormControl>
+                      <Switch
+
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem className={formItemBaseCls}>
+                    <FormLabel className={formLabelBaseCls}>标题</FormLabel>
+                    <FormControl>
+                      <Input placeholder="请输入标题" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="placeholder"
+                render={({ field }) => (
+                  <FormItem className={formItemBaseCls}>
+                    <FormLabel className={formLabelBaseCls}>提示文字</FormLabel>
+                    <FormControl>
+                      <Input placeholder="请输入提示文字" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   )
 }
 
--- src/questions/OpenQuestion/index.tsx
+++ src/questions/OpenQuestion/index.tsx
@@ -1,7 +1,5 @@
 import { HTMLAttributes } from 'react'
-import { useQuestionStore } from '@/store/useQuestionStore'
-import OpenQuestionEdit from './Edit'
-import OpenQuestionPreview from '@/questions/OpenQuestion/Preview'
+import { Textarea } from '@/components/ui/textarea'
 
 export interface OpenQuestionProps extends HTMLAttributes<HTMLDivElement> {
   title: string;
@@ -12,17 +10,12 @@ export interface OpenQuestionProps extends HTMLAttributes<HTMLDivElement> {
 // fixme: dynamic switch between edit and preview mode will cause useOutsideClick work not correctly
 const OpenQuestion = (props: OpenQuestionProps) => {
   const { id, title, placeholder, ...restProps } = props
-  const activeQuestionId = useQuestionStore((state) => state.activeQuestionId)
-  const editable = activeQuestionId === id
-  const renderChildren = () => {
-    if (editable) {
-      return <OpenQuestionEdit title={title} id={id} placeholder={placeholder}/>
-    }
-    return <OpenQuestionPreview title={title} placeholder={placeholder}/>
-  }
   return (
     <div {...restProps}>
-      {renderChildren()}
+      <div className={'flex items-center justify-between'}>
+        <div className={'text-lg font-medium flex-1'}>{title}</div>
+      </div>
+      <Textarea placeholder={placeholder} className={'resize-none mt-2'}/>
     </div>
   )
 }
