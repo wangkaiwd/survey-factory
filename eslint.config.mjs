b89f1bc6b286132043fac6777af9276c25efa76d
wangkaiwd <1162486176@qq.com>
2025-06-26 13:38:56 +0800

    feat: remove unnecessary configuration block from eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,8 +19,6 @@ const eslintConfig = [
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
       },
-    },
-    {
       ignore: ['.lintstagedrc.mjs', 'eslint.config.mjs'],
     },
   ),
