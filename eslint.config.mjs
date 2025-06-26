dd1194a8d3196662f315341e87c9ed8ead4cdd30
wangkaiwd <1162486176@qq.com>
2025-06-26 11:47:01 +0800

    feat: add eslint prettier commitlint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,15 +10,20 @@ const compat = new FlatCompat({
 })
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    rules: {
-      '@typescript-eslint/no-empty-object-type': 'off',
-      'react-hooks/exhaustive-deps': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': ['error', { 'ignoreRestSiblings': true }],
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  ...compat.config(
+    {
+      rules: {
+        '@typescript-eslint/no-empty-object-type': 'off',
+        'react-hooks/exhaustive-deps': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
+      },
     },
-  },
+    {
+      ignore: ['.lintstagedrc.mjs', 'eslint.config.mjs'],
+    },
+  ),
 ]
 
 export default eslintConfig
